3959b35893ee424b83959eb8ca1d181e24f4f5f8
ray sonntag <raysonntag@yahoo.com>
2019-10-14 21:45:05 -0400

    practice start

---
+++ practice.js
@@ -0,0 +1,22 @@
+var a;var b;var c;var d;var i;var r;var t;var rr;var may=[0];
+console.log("ray was here for the moment");
+function funk() {
+    for(i=0;i<13;i++) {
+    var rand=Math.floor(Math.random()*100);
+    r=rand;
+    may[i]=r;
+    document.getElementById("bo1").innerHTML=may[0];
+    document.getElementById("bo2").innerHTML=may[1];
+    document.getElementById("bo3").innerHTML=may[2];
+    document.getElementById("bo4").innerHTML=may[3];
+    document.getElementById("bo5").innerHTML=may[4];
+    document.getElementById("bo6").innerHTML=may[5];
+    document.getElementById("bo7").innerHTML=may[6];
+    document.getElementById("bo8").innerHTML=may[7];
+    document.getElementById("bo9").innerHTML=may[8];
+    document.getElementById("b10").innerHTML=may[9];
+    document.getElementById("b11").innerHTML=may[10];
+    document.getElementById("b12").innerHTML=may[11];
+    console.log(may[0]);
+}
+}
