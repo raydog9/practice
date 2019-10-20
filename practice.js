5b876b4146061ba68e8c203f4072269853195673
ray sonntag <raysonntag@yahoo.com>
2019-10-20 04:08:37 -0400

    even numbers and odd numbers in the console

--- practice.js
+++ practice.js
@@ -6,16 +6,25 @@ function funk() {
   for (i=0;i<10;i++) { 
     var rand=Math.floor(Math.random()*100);
     may[i]=rand;
+    ray[i]=rand;
     if (rand%2==1) {
       rr=rand;may[i]=rr;
-      console.log(rr + (" this a odd number"));
+      console.log(rr + (" this a odd number" + (i)))  ;
     }
-    else {
-    console.log(may[i] + (" this is an even number"));
-    }
-  }
+    
+   }
+  console.log("\n");
 }
 function fun1 () {
-  console.log("\n",may[9]); 
- 
+  for (d=0;d<10;d++) {
+     console.log(ray[d]); 
+  }
+  console.log("\n");
+}
+function fun2 () {
+  for (c=0;c<10;c++) {
+    if (ray[c]%2==0) {
+      console.log(ray[c] + ("this is an even number"));
+    }
+  }
 }
