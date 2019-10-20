6342edcf5f229fb2e57a9f1ca4a24119b77c31e0
ray sonntag <raysonntag@yahoo.com>
2019-10-19 23:51:35 -0400

    changes from tab

--- practice.js
+++ practice.js
@@ -1,27 +1,21 @@
-var a;var b;var c;var d;var i;var r;var t;var rr;var may=[0];arry=[0];
+var a;var b;var c;var d;var i;var r;var t;var rr;var may=[0];arry=[0];ray=[0];
 
 a=0;
 function funk() {
   c=0;
-  for (i=0;i<10;i++) {
- var rand=Math.floor(Math.random()*100);
- may[i]=rand
-if (rand%2==1) {
-    a=rand;arry[i]=a;
- document.getElementById("bo1").innerHTML=a;
- document.getElementById("bo2").innerHTML=("THIS IS AN ODD NUMBER");
-console.log(arry[i]+"\n");
-c++;
-console.log(c);
+  for (i=0;i<10;i++) { 
+    var rand=Math.floor(Math.random()*100);
+    may[i]=rand;
+    if (rand%2==1) {
+      rr=rand;may[i]=rr;
+      console.log(rr + (" this a odd number"));
+    }
+    else {
+    console.log(may[i] + (" this is an even number"));
+    }
+  }
 }
-else {
-document.getElementById("bo1").innerHTML=rand;
-document.getElementById("bo2").innerHTML=("this is an even number")
-d="";
-d++;
-document.getElementById("bo3").innerHTML=d;
-
-}
-
-}
+function fun1 () {
+  console.log("\n",may[9]); 
+ 
 }
