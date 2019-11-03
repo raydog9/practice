203ae243a8d3f9676a02c773035c2f4d199f7dd3
ray sonntag <raysonntag@yahoo.com>
2019-11-03 18:30:55 -0500

    1103

--- practice.js
+++ practice.js
@@ -1,16 +1,24 @@
- var a,b,c,d,e,i,n,o,r,s,t,aray=[],may=[],ray=[],say=[];
+var a,b,c,d,e,f,i,n,o,r,s,t,aray=[],may=[],ray=[],say=[];
 
- var inp1=document.getElementById("inp1");
- var inp2=document.getElementById("inp2");
- var sum=document.getElementById("sum");
 
- inp1.addEventListener("input",fun1);
- inp2.addEventListener("input",fun1);
-
- function fun1() {
-   var one=parseFloat(inp1.value) || 0;
-   var two=parseFloat(inp2.value) || 0;
-   sum.innerHTML= one + two;
-   
-   
- }
+bu1.addEventListener("click",fun1);
+bu2.addEventListener("click",fun2);
+bu3.addEventListener("click",fun3);
+function fun1() {
+  for (i=0;i<10;i++) {
+      var rand=Math.floor(Math.random() *100);
+        aray.push(rand);
+      if(rand%2==0) {
+      may.push(rand);
+  }
+  else {
+    ray.push(rand);
+       }
+  }
+  document.getElementById("bo1").innerHTML=aray;
+}function fun2() {
+  document.getElementById("bo2").innerHTML=may;
+}
+  function fun3() {
+    document.getElementById("bo3").innerHTML=ray;
+  }
