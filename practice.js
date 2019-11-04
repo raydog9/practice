72dbb4aa72775ea3dec8e06b12208c7dde8ebc37
ray sonntag <raysonntag@yahoo.com>
2019-11-04 18:41:40 -0500

    update

--- practice.js
+++ practice.js
@@ -5,10 +5,12 @@ var in2=document.getElementById("in2");
 var he1=document.getElementById("he1");
 var bo1=document.getElementById("bo1");
 var bo2=document.getElementById("bo2");
-
+var bo3=document.getElementById("bo3");
 
 in1.addEventListener("input",fun1);
 in2.addEventListener("input",fun1);
+bu1.addEventListener("click",fun2);
+
 function fun1() {
     var one=parseFloat(in1.value) || 0;
     var two=parseFloat(in2.value) || 0;
@@ -16,3 +18,8 @@ function fun1() {
      bo1.innerHTML=one;
      bo2.innerHTML=two;
 } 
+
+function fun2() {
+    var rand=Math.floor(Math.random()*10);
+    bo3.innerHTML=rand;
+}
